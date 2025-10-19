553ec6a221a7164a68cb87e03f4f7498a20699e5
Tej <reddytej15@gmail.com>
2025-10-19 21:09:42 +0530

    feat:port assigned

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def handle_global_exception(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
